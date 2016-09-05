dcf4bd4116cb328ebf9d8c0ffcc89190bb1d996b
aM3z <amezolma@plu.edu>
2016-09-05 14:11:44 -0700

    clear up directory tree

--- src/ArrayList.ts
+++ src/ArrayList.ts
@@ -3,19 +3,19 @@
 namespace Collections {
 	/**
 	 * Resizable-array implementation of the List interface.
-	 * 
+	 *
 	 * @export
 	 * @class ArrayList
 	 * @implements {List<E>}
 	 * @template E
-	 * 
+	 *
 	 * @author Miguel Amezola <amezolma@plu.edu>
 	 */
 	export class ArrayList<E> implements List<E> {
 
 		/**
 		 * The array buffer into which the elements of the ArrayList are stored.
-		 * 
+		 *
 		 * @private
 		 * @type {E[]}
 		 */
@@ -23,251 +23,151 @@ namespace Collections {
 
 		/**
 		 * Creates an empty list.
-		 * 
+		 *
 		 */
 		public constructor() {
 			this._elementData = [];
 		}
 
-		/**
-		 * Appends the specified element to the end of this list.
-		 * If optional parameter index is not undefined, inserts the specified element at the specified position in this list. Shifts the element currently at that position (if any) and any subsequent elements to the right (adds one to their indices). 
-		 * 
-		 * @param {E} element - element to be inserted
-		 * @param {number} [index] - index at which the specified element is to be inserted
-		 * @returns {boolean} true if this ArrayList changed as a result of the call
-		 * @throws {RangeError} if the index is out of range (index < 0 || index > size())
-		 */
-		public add(element: E, index: number): boolean {
-			// if both `element` and `index` parameters are passed
-			if(element && typeof index === "number" ) {
-				// throw RangeError if the index is out of range
-				if(index < 0 || index > this.size()) {
-					throw new RangeError("index is out of range (index < 0 || index > size())");
-				}
-				// make a copy of the left side of `_elementData` array
-				// do not include the element currently at position `index` 
-				let left: E[] = this._elementData.slice(0, index);
-				// make a copy of the right side of `_elementData` array
-				// include the element currently at position `index`
-				let right: E[] = this._elementData.slice(index);
-				// wrap element in array 
-				let wrappedElement: E[] = [element];
-				// concatenate left, wrappedElement, and right arrays
-				// assign new array to `_elementData`
-				this._elementData = left.concat([element], right);
-				// true if this ArrayList changed 
-				return true;
-			// if `element` is the only parameter passed
-			} else if(element) {
-				// push element 
-				this._elementData.push(element);
-				// true if this ArrayList changed
-				return true;
-			}
-			// false if this ArrayList did not changed
-			return false;
-		}
-		/**
-		 * Removes all of the elements from this list. The list will be empty after this call returns.
-		 */
-		public clear(): void {
-			this._elementData.length = 0;
-		}
-		/**
-		 * Returns true if this collection contains the specified element.
-		 * 
-		 * @param {Object} o - element whose presence in this collection is to be tested
-		 * @returns {boolean} true if this collection contains the specified element
-		 */
-		public contains(element: E): boolean {
-			// true if `o` is in `_elementData`
-			if(this._elementData.indexOf(element) >= 0) {
-				return true;
-			}
-			// false if `o` is not in `_elementData`
-			return false;
-		 }
+		// BASIC OPERATIONS
 
 		/**
-		 * Compares the specified object with this collection for equality.
-		 * 
-		 * @param {Object} o - object to be compared for equality with this collection
-		 * @returns {boolean} true if the specified object is equal to this collection
+		 * Adds an element to this collection.
+		 *
+		 * @param {E} element - element to be added
+		 * @param {number} [index] - index of element to be added
+		 * @returns {boolean} true if element is successfully added
 		 */
+		add(element: E, index?: number): boolean;
 		/**
-		public equals(o: Object): boolean {
-			if(this.containsAll(o as ArrayList<E>) && (o as ArrayList<E>).containsAll(this)) {
-				return true;
-			} 
-			return false; 
-		}
-		*/
+		 * Returns true if a target element is in this collection.
+		 *
+		 * @param {E} element - target element
+		 * @returns {boolean} true if target element is in this collection
+		 */
+		contains(element: E): boolean;
+		/**
+		 * Returns true if this collection contains no elements.
+		 *
+		 * @returns {boolean} true if this collection contains no elements
+		 */
+		isEmpty(): boolean;
+		/**
+		 * Removes an element from this collection.
+		 *
+		 * @param {(E | number)} element - the Object or index of element to be removed
+		 * @returns {boolean} true if element is successfully removed
+		 */
+		remove(element: E | number): boolean;
+		/**
+		 * Returns the number of elements in this collection.
+		 *
+		 * @returns {number} the number of elements in this collection
+		 */
+		size(): number;
+
+		// BULK OPERATIONS
+
+		/**
+		 * Removes all elements from this collection.
+		 *
+		 */
+		clear(): void;
+
+
+		// ARRAY OPERATIONS
+
+		/**
+		 * Translates the contents of this collection into an array.
+		 *
+		 * @returns {E[]} the contents of this collection as an array
+		 */
+		toArray(): E[];
+
+
+		// POSITIONAL ACCESS
 
 		/**
 		 * Returns the element at the specified position in this list.
-		 * 
+		 *
 		 * @param {number} index - index of the element to return
-		 * @returns {E} the element at the specified position in this list
+		 * @returns {E}	the element at the specified position in this list
 		 * @throws {RangeError} if the index is out of range (index < 0 || index >= size())
 		 */
-		public get(index: number): E {
-			// throw RangeError if the index is out of range
-			if(index < 0 || index >= this.size()) {
-				throw new RangeError("index is out of range (index < 0 || index >= size())");
-			}
-			// return the element at the specified position in this list
-			return this._elementData[index];
-		}
-
-		// not implemented yet
-		public hashCode(): number { 
-			return 0;        
-		}
-
-
-		/**
-		 * Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element. More formally, returns the lowest index i such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there is no such index.
-		 * 
-		 * @param {Object} o - element to search for
-		 * @returns {number} the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element
-		 */
-		public indexOf(o: Object): number { 
-			// default to -1
-			let index: number = -1;
-			// iterate through `_elementData`
-			loop: {
-			  for (let i: number = 0; i < this.size(); i++) {
-				// if `o` object is located in `_elementData`
-				if (o === this._elementData[i]) {
-				  // store the index of the first occurrence of the specified element
-				  index = i;
-				  // break out of this loop
-				  break loop;
-				}
-			  }
-			}
-		
-			return index;
-		}
-		/**
-		 * Returns true if this list contains no elements.
-		 * 
-		 * @returns {boolean} true if this list contains no elements.
-		 */
-		public isEmpty(): boolean {
-			return this.size() === 0; 
-		}
-		/**
-		 * Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element. More formally, returns the highest index i such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there is no such index.
-		 * 
-		 * @param {Object} o - element to search for
-		 * @returns {number} the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element
-		 */
-		public lastIndexOf(o: Object): number { 
-			// default to -1
-			let index: number = -1;
-			// iterate through `_elementData`
-			loop: {
-			  for (let i: number = this.size() - 1; i >= 0; i--) {
-				// if `o` object is located in `_elementData`
-				if (o === this._elementData[i]) {
-				  // store the index of the last occurrence of the specified element
-				  index = i;
-				  // break out of this loop
-				  break loop;
-				}
-			  }
-			}
-		
-			return index;        
-		}
-
-		public remove(element: number | any): E | boolean { 
-			// remove the element at the specified position
-			if(typeof element === "number") {
-
-				// throw RangeError if the index is out of range
-				if(element < 0 || element >= this.size()) {
-					throw new RangeError("index is out of range (index < 0 || index >= size())");
-				}
-				// make a copy of the left side of `_elementData` array
-				// do not include the element currently at position `index` 
-				let left: E[] = this._elementData.slice(0, element);
-				// make a copy of the right side of `_elementData` array
-				// do not include the element currently at position `index`
-				let right: E[] = this._elementData.slice(element + 1);
-				// wrap element in array 
-				let removedElement: E = this.get(element);
-				// concatenate left, wrappedElement, and right arrays
-				// assign new array to `_elementData`
-				this._elementData = left.concat(right);
-				// return the element that was removed from the list
-				return removedElement;
-
-			// remove the first occurrence of the specified element from this list, if it is present
-			} else {
-				// get index of the first occurrence of the specified element
-				let index: number = this.indexOf(element);
-				// if element found them remove it and return true
-				if(index >= 0) {
-					this.remove(index);
-					return true;
-				}
-				
-				return false;
-			}
-		}
-
-
+		get(index: number): E;
 		/**
 		 * Replaces the element at the specified position in this list with the specified element.
-		 * 
+		 *
 		 * @param {number} index - index of the element to replace
 		 * @param {E} element - element to be stored at the specified position
 		 * @returns {E} the element previously at the specified position
+		 * @throws {RangeError} if the index is out of range (index < 0 || index >= size())
 		 */
-		public set(index: number, element: E): E {
-			// if the index is out of range
-			if(index < 0 || index >= this.size()) {
-				throw new RangeError("index is out of range (index < 0 || index >= size())");
-			}
-			// store element previously at the specified position    
-			let previousElement: E = this._elementData[index];
-			// replace the element at the specified position with the specified element
-			this._elementData[index] = element;
-			// return the element previously at the specified position        
-			return previousElement;
-		}
+		set(index: number, element: E): E;
 		/**
-		 * Returns the number of elements in this collection. 
-		 * 
-		 * @returns {number} the number of elements in this collection
+		 * Inserts the specified element at the specified position in this list.
+		 * Shifts the element currently at that position (if any) and any subsequent elements to the right (adds one to their indices).
+		 *
+		 * @param {E} element - element to be inserted
+		 * @param {number} index - index at which the specified element is to be inserted
+		 * @returns {boolean} true if element was successfully added
+		 * @throw {RangeError} if the index is out of range (index < 0 || index > size())
 		 */
-		public size(): number {return this._elementData.length;}
-		// not implementated yet
-		public subList(fromIndex: number, toIndex: number): List<E> { return null; }
+		add(element: E, index: number): boolean;
 		/**
-		 * Returns an array containing all of the elements in this collection. If this collection makes any guarantees as to what order its elements are returned by its iterator, this method must return the elements in the same order.
-		 * The returned array will be "safe" in that no references to it are maintained by this collection. (In other words, this method must allocate a new array even if this collection is backed by an array). The caller is thus free to modify the returned array.
-		 * This method acts as bridge between array-based and collection-based APIs.
-		 * 
-		 * @returns {Object[]} an array containing all of the elements in this collection
+		 * Removes the element at the specified position in this list.
+		 * Shifts any subsequent elements to the left (subtracts one from their indices).
+		 * Returns the element that was removed from the list.
+		 *
+		 * @param {number} index - the index of the element to be removed
+		 * @returns {boolean} true if element was successfully removed
 		 */
-		public toArray(): Object[] {
-			// instantiate new array
-			let newArray: E[] = [];
-			// push each element in `_elementData` to `newArray` 
-			this._elementData.forEach((element) => newArray.push(element) );
-			// return new array
-			return newArray; 
-		}
+		remove(index: number): boolean;
 		/**
-		 * Returns a string representation of this collection. 
-		 * The string representation consists of a list of the collection's elements in the order they are returned by its iterator, enclosed in square brackets ("[]"). 
-		 * Adjacent elements are separated by the characters ", " (comma and space). 
+		 * Inserts all of the elements in the specified collection into this list at the specified position (optional operation).
+		 * Shifts the element currently at that position (if any) and any subsequent elements to the right (increases their indices).
+		 *
+		 * @param {number} index - index at which to insert the first element from the specified collection
+		 * @param {Collection<E>} c - collection containing elements to be added to this list
+		 * @returns {boolean} true if this list changed as a result of the call
+		 */
+		addAll(index: number, c: Collection<E>): boolean;
+
+		// SEARCH
+
+		/**
+		 * Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+		 *
+		 * @param {E} element - element to search for
+		 * @returns {number} the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element
+		 */
+		indexOf(element: E): number;
+		/**
+		 * Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element.
+		 *
+		 * @param {E} element - element to search for
+		 * @returns {number} the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element
+		 */
+		lastIndexOf(element: E): number;
+
+		// RANGE-VIEW
+
+		/**
+		 * Returns a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive. (If fromIndex and toIndex are equal, the returned list is empty.)
+		 * The returned list is backed by this list, so non-structural changes in the returned list are reflected in this list, and vice-versa.
+		 *
+		 * @param {number} from - low endpoint (inclusive) of the subList
+		 * @param {number} to - high endpoint (exclusive) of the subList
+		 * @returns {List<E>} a view of the specified range within this list
+		 */
+		subList(from: number, to: number): List<E>;
+
+		/**
+		 * Returns a string representation of this collection.
+		 * The string representation consists of a list of the collection's elements in the order they are returned by its iterator, enclosed in square brackets ("[]").
+		 * Adjacent elements are separated by the characters ", " (comma and space).
 		 * Elements are manually converted to strings by `Object + ", "`.
-		 * 
+		 *
 		 * @returns {string} a string representation of this collection
 		 */
 		public toString(): string {
--- src/List.ts
+++ src/List.ts
@@ -4,10 +4,10 @@ namespace Collections {
     /**
      * A ordered collection. Lists may include duplicate elements.
 	 * In addition to all to the operations inherited from Collection, the List interface includes operations for:
-     * 
+     *
 	 * Positional Access: manipulate elements based on their numerical position in the list.
 	 * Search: search for a specified object in the list and return its numerical position.
-	 * Range-view: perform arbitrary range operations on the list. 
+	 * Range-view: perform arbitrary range operations on the list.
 	 *
      * @export
      * @interface List
@@ -16,11 +16,11 @@ namespace Collections {
      */
     export interface List<E> extends Collection<E> {
 
-        // ========== POSITIONAL ACCESS ==========
+    // POSITIONAL ACCESS
 
 		/**
 		 * Returns the element at the specified position in this list.
-		 * 
+		 *
 		 * @param {number} index - index of the element to return
 		 * @returns {E}	the element at the specified position in this list
 		 * @throws {RangeError} if the index is out of range (index < 0 || index >= size())
@@ -28,65 +28,65 @@ namespace Collections {
 		get(index: number): E;
 		/**
 		 * Replaces the element at the specified position in this list with the specified element.
-		 * 
+		 *
 		 * @param {number} index - index of the element to replace
-		 * @param {E} element - element to be stored at the specified position 
+		 * @param {E} element - element to be stored at the specified position
 		 * @returns {E} the element previously at the specified position
 		 * @throws {RangeError} if the index is out of range (index < 0 || index >= size())
 		 */
 		set(index: number, element: E): E;
 		/**
-		 * Inserts the specified element at the specified position in this list. 
+		 * Inserts the specified element at the specified position in this list.
 		 * Shifts the element currently at that position (if any) and any subsequent elements to the right (adds one to their indices).
-		 * 
+		 *
 		 * @param {E} element - element to be inserted
 		 * @param {number} index - index at which the specified element is to be inserted
-		 * @returns {boolean} true if element was successfully added 
+		 * @returns {boolean} true if element was successfully added
 		 * @throw {RangeError} if the index is out of range (index < 0 || index > size())
 		 */
 		add(element: E, index: number): boolean;
 		/**
-		 * Removes the element at the specified position in this list. 
-		 * Shifts any subsequent elements to the left (subtracts one from their indices). 
+		 * Removes the element at the specified position in this list.
+		 * Shifts any subsequent elements to the left (subtracts one from their indices).
 		 * Returns the element that was removed from the list.
-		 * 
+		 *
 		 * @param {number} index - the index of the element to be removed
-		 * @returns {boolean} true if element was successfully removed 
+		 * @returns {boolean} true if element was successfully removed
 		 */
 		remove(index: number): boolean;
 		/**
-		 * Inserts all of the elements in the specified collection into this list at the specified position (optional operation). 
+		 * Inserts all of the elements in the specified collection into this list at the specified position (optional operation).
 		 * Shifts the element currently at that position (if any) and any subsequent elements to the right (increases their indices).
-		 * 
+		 *
 		 * @param {number} index - index at which to insert the first element from the specified collection
 		 * @param {Collection<E>} c - collection containing elements to be added to this list
 		 * @returns {boolean} true if this list changed as a result of the call
 		 */
 		addAll(index: number, c: Collection<E>): boolean;
 
-        // ========== SEARCH ==========
+    // SEARCH
 
 		/**
 		 * Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
-		 * 
+		 *
 		 * @param {E} element - element to search for
 		 * @returns {number} the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element
 		 */
 		indexOf(element: E): number;
 		/**
 		 * Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element.
-		 * 
+		 *
 		 * @param {E} element - element to search for
 		 * @returns {number} the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element
 		 */
 		lastIndexOf(element: E): number;
 
-        // ========== RANGE-VIEW ==========
+    // RANGE-VIEW
 
 		/**
-		 * Returns a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive. (If fromIndex and toIndex are equal, the returned list is empty.) 
-		 * The returned list is backed by this list, so non-structural changes in the returned list are reflected in this list, and vice-versa. 
-		 * 
+		 * Returns a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive. (If fromIndex and toIndex are equal, the returned list is empty.)
+		 * The returned list is backed by this list, so non-structural changes in the returned list are reflected in this list, and vice-versa.
+		 *
 		 * @param {number} from - low endpoint (inclusive) of the subList
 		 * @param {number} to - high endpoint (exclusive) of the subList
 		 * @returns {List<E>} a view of the specified range within this list
